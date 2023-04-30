29a177d56576dfbfdc6eac9b4415ab64a1b0cebc
Eujeen Han <eujeenhan@gmail.com>
2023-05-01 07:08:25 +0900

    Update ProductEntity with @Entity and @Table annotations

--- src/main/java/com/example/shop/model/ProductEntity.java
+++ src/main/java/com/example/shop/model/ProductEntity.java
@@ -4,7 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
 // Lombok annotations to generate boilerplate code
 @Builder
 @NoArgsConstructor
@@ -13,6 +21,9 @@ import lombok.NoArgsConstructor;
 public class ProductEntity {
 
     // Primary key for the ProductEntity table
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
     private int id;
 
     // ID of the user who posted the product
