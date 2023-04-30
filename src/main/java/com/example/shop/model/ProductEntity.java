2345ab1249e87b3e4e97463305e53c85b680613b
Eujeen Han <eujeenhan@gmail.com>
2023-04-30 23:24:23 +0900

    Add ProductEntity class as a combined model and entity class

---
+++ src/main/java/com/example/shop/model/ProductEntity.java
@@ -0,0 +1,29 @@
+package com.example.shop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Lombok annotations to generate boilerplate code
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ProductEntity {
+
+    // Primary key for the ProductEntity table
+    private String id;
+
+    // ID of the user who posted the product
+    private String userId;
+
+    // Title or name of the product
+    private String title;
+
+    // Material used to make the product
+    private String material;
+
+    // Price of the product
+    private Double price;
+}
