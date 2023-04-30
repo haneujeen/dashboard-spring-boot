a91977f7505e123826765b310dae7077ae71149d
Eujeen Han <eujeenhan@gmail.com>
2023-05-01 02:50:16 +0900

    Add new endpoints to TestController

--- src/main/java/com/example/shop/controller/TestController.java
+++ src/main/java/com/example/shop/controller/TestController.java
@@ -1,6 +1,12 @@
 package com.example.shop.controller;
 
+import com.example.shop.dto.ResponseDTO;
+import com.example.shop.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // This class is a REST controller that handles requests for the "/api/test" endpoint
 @RestController
@@ -23,5 +29,33 @@ public class TestController {
     @GetMapping("/message")
     public String getMessageWithRequestParam(@RequestParam(required = false) String message) {
         return "This is a test message with request parameter: " + message;
+    }
+
+    // A GET request that returns a message with a request body parameter
+    @GetMapping("/message")
+    public String getMessageWithRequestBody(@RequestBody TestRequestBodyDTO requestBody) {
+        return "This is a test message with id '" + requestBody.getId() + "' and message " + requestBody.getMessage();
+    }
+
+    // A GET request that returns a response DTO containing a list of strings
+    @GetMapping("/message")
+    public ResponseDTO<String> getMessageWithResponseBody() {
+        List<String> list = new ArrayList<>();
+        list.add("This is the first string in the list");
+        list.add("This is the second string in the list");
+
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return response;
+    }
+
+    // A GET request that returns a response entity with a status containing a response DTO
+    @GetMapping("/message")
+    public ResponseEntity<?> getMessageWithResponseEntity() {
+        List<String> list = new ArrayList<>();
+        list.add("This is the first string in the list");
+        list.add("This is the second string in the list");
+
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(response);
     }
 }
