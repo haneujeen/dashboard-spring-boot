08a3ce784f19a9405787a5f6e2a8d1dfad548748
Eujeen Han <eujeenhan@gmail.com>
2023-05-02 00:50:58 +0900

    Add endpoint for creating a new product in ProductController

--- src/main/java/com/example/shop/controller/ProductController.java
+++ src/main/java/com/example/shop/controller/ProductController.java
@@ -12,12 +12,11 @@ import com.example.shop.model.ProductEntity;
 import com.example.shop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/product")
@@ -64,5 +63,51 @@ public class ProductController {
         ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 
         return ResponseEntity.ok().body(response);
+    }
+
+    /**
+     * Endpoint for creating a new product.
+     *
+     * @param dto the ProductDTO object to be created
+     * @return a ResponseEntity with a list of ProductDTO objects wrapped in a ResponseDTO object
+     */
+    @PostMapping()
+    public ResponseEntity<?> createProduct(@RequestBody ProductDTO dto) {
+        try {
+            // Creating a test user ID to assign to the created product entity
+            String testUserId = "test-user";
+
+            // Convert the received ProductDTO object to a ProductEntity object
+            ProductEntity entity = ProductDTO.toEntity(dto);
+
+            // Set the ID of the ProductEntity object to null to ensure it gets a new ID
+            entity.setId(null);
+
+            // Set the user ID of the ProductEntity object to the test user ID
+            entity.setUserId(testUserId);
+
+            // Call the ProductService's create method to create the new product entity in the database
+            List<ProductEntity> entities = service.create(entity);
+
+            // Convert the created ProductEntity objects to ProductDTO objects
+            List<ProductDTO> dtos = entities.stream()
+                    .map(ProductDTO::new)
+                    .collect(Collectors.toList());
+
+            // Create a ResponseDTO object to wrap the ProductDTO objects and send it as an HTTP response
+            ResponseDTO<ProductDTO> response = ResponseDTO.<ProductDTO>builder()
+                    .data(dtos)
+                    .build();
+
+            // Return an HTTP response with a status code of 200 (OK) and the ResponseDTO object as the body
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            // If an exception is caught, create a ResponseDTO object with an error message and send it as a bad request
+            String errorMessage = e.getMessage();
+            ResponseDTO<ProductDTO> response = ResponseDTO.<ProductDTO>builder()
+                    .errorMessage(errorMessage)
+                    .build();
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 }
