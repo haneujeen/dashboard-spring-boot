0070da5a10c3ce6a4c2a8c1acfd1019b38c6509b
Eujeen Han <eujeenhan@gmail.com>
2023-05-01 18:51:26 +0900

    Add custom query to retrieve products by user ID in ProductRepository

--- src/main/java/com/example/shop/persistence/ProductRepository.java
+++ src/main/java/com/example/shop/persistence/ProductRepository.java
@@ -2,6 +2,14 @@ package com.example.shop.persistence;
 
 import com.example.shop.model.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, String> {
+
+    List<ProductEntity> findByUserId(String userId);
+
+    // Custom query to retrieve a list of products for a given user ID:
+    // @Query("select * from Product p where p.userId = ?1")
 }
