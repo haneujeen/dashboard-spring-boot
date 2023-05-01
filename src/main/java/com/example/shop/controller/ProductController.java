9e5594220b9965d271d35bb2eaddc3e4316cdffa
Eujeen Han <eujeenhan@gmail.com>
2023-05-01 18:33:00 +0900

    Add methods to get message and title response from ProductService

--- src/main/java/com/example/shop/controller/ProductController.java
+++ src/main/java/com/example/shop/controller/ProductController.java
@@ -1,3 +1,9 @@
+/**
+ * The ProductController class is a REST controller that handles HTTP requests related to products.
+ * It interacts with the ProductService to perform CRUD operations on the ProductEntity objects
+ * and convert them to/from ProductDTO objects.
+ */
+
 package com.example.shop.controller;
 
 import com.example.shop.dto.ProductDTO;
@@ -36,12 +42,25 @@ public class ProductController {
     private ProductService service;
 
     // Test method to get message response
-    @GetMapping("/message")
+    @GetMapping("/service-message")
     public ResponseEntity<?> getMessage() {
         String message = service.getMessage();
 
         List<String> list = new ArrayList<>();
         list.add(message);
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    // Retrieves the title of a ProductEntity object from the database through the ProductService,
+    // wraps it in a ResponseDTO object and sends it as an HTTP response.
+    @GetMapping("/service-title")
+    public ResponseEntity<?> getTitle() {
+        String title = service.getProductTitle();
+
+        List<String> list = new ArrayList<>();
+        list.add(title);
         ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 
         return ResponseEntity.ok().body(response);
