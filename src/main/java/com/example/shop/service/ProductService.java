3515d749206462cfdfa50fc6858a94af832cb7ef
haneujeen <eujeenhan@gmail.com>
2023-06-11 23:25:02 +0900

    Update `update` in ProductService

--- src/main/java/com/example/shop/service/ProductService.java
+++ src/main/java/com/example/shop/service/ProductService.java
@@ -7,53 +7,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
-// This ProductService class is a service layer component
-// that interacts with the persistence layer through the injected ProductRepository.
+/**
+ * This service class interacts with the product controller and product repository. This includes:
+ *
+ * getService(), getTitle(): Test a service class
+ * create(final ProductEntity entity): Creates a new product
+ * update(final ProductEntity entity): Updates an existing product
+ * delete(String productId): Deletes a product
+ * validate(final ProductEntity entity): Validates a product
+ * retrieve(String productId): Retrieves a product
+ */
 @Slf4j
 @Service
 public class ProductService {
 
-    // Returns a test message
     public String getMessage() {
-        return "This is a test message from the ProductService class.";
+        return "ProductService test";
     }
 
-    @Autowired
-    private ProductRepository repository;
-
-    // Creates a new ProductEntity object, saves it to the repository,
-    // retrieves the saved entity by ID, and returns its title
+    // Create a new product entity only with its title, find it by ID, and return the title
     public String getProductTitle() {
-        ProductEntity entity = ProductEntity.builder().title("Title of First Product").build();
+        ProductEntity entity = ProductEntity.builder().title("Sample Product").build();
         repository.save(entity);
 
         ProductEntity savedEntity = repository.findById(entity.getId()).get();
         return savedEntity.getTitle();
     }
 
+    @Autowired
+    private ProductRepository repository;
+
     /**
-     * Creates a new product entity and saves it to the database.
-     * Returns a list of all products with the same user ID as the created entity.
+     * Create and save a new product entity.
      *
      * @param entity The product entity to create and save.
      * @return A list of all products with the same user ID as the created entity.
-     * @throws RuntimeException if the entity is null or if the user ID is null.
      */
     public List<ProductEntity> create(final ProductEntity entity) {
-
-        // Validate the entity before saving it to the database
         validate(entity);
-
-        // Save the entity to the database
         repository.save(entity);
+        log.info("Product ID {} created", entity.getId());
 
-        // Log the ID of the created product entity
-        log.info("Product with ID {} created", entity.getId());
-
-        // Return a list of all products with the same user ID as the created entity
-        return repository.findByUserId(entity.getUserId());
+        return repository.findByUserId(entity.getUser().getId());
     }
 
     /**
@@ -62,76 +58,66 @@ public class ProductService {
      *
      * @param entity The product entity to update.
      * @return A list of all products with the same user ID as the updated entity.
-     * @throws RuntimeException if the entity is null or if the ID or user ID is null.
      */
     public List<ProductEntity> update(final ProductEntity entity) {
-
-        // Validate the entity before updating it in the database
         validate(entity);
 
-        // Retrieve the existing entity from the database
-        Optional<ProductEntity> optionalEntity = repository.findById(entity.getId());
+        ProductEntity currentEntity = repository.findById(entity.getId())
+                .orElseThrow(() -> new RuntimeException("Product not found"));
 
-        // Update the title of the entity if it exists in the database
-        optionalEntity.ifPresent(product -> {
-            product.setTitle(entity.getTitle());
-            repository.save(product);
-        });
+        currentEntity.setTitle(entity.getTitle());
+        currentEntity.setUser(entity.getUser());
+        if (entity.getMaterial() != null) {
+            currentEntity.setMaterial(entity.getMaterial());
+        }
+        if (entity.getPrice() != null) {
+            currentEntity.setPrice(entity.getPrice());
+        }
+        if (entity.getCompany() != null) {
+            currentEntity.setCompany(entity.getCompany());
+        }
+        repository.save(currentEntity);
 
-        // Return a list of all products with the same user ID as the updated entity
-        return retrieve(entity.getUserId());
+        return retrieve(entity.getUser().getId());
     }
 
+    /**
+     * Deletes product from the database.
+     *
+     * @param entity The product entity to delete.
+     * @return A list of all products with the same user ID as the deleted entity.
+     * @throws RuntimeException if an error occurs.
+     */
+    public List<ProductEntity> delete(final ProductEntity entity) {
+        validate(entity);
+
+        try {
+            repository.delete(entity);
+        } catch (Exception e) {
+            log.error("Failed deleting product ID " + entity.getId() + ": " + e.getMessage());
+            throw new RuntimeException("Error deleting product ID " + entity.getId() + ": " + e.getMessage());
+        }
+
+        return retrieve(entity.getUser().getId());
+    }
 
     /**
-     * Validates a product entity.
+     * Validates a product entity by itself and user.
      *
      * @param entity The product entity to validate.
      * @throws RuntimeException if the entity is null or if the user ID is null.
      */
-    private static void validate(ProductEntity entity) {
-        // Throw a runtime exception if the entity is null
+    private static void validate(final ProductEntity entity) {
         if (entity == null) {
-            log.warn("The product entity is null");
+            log.warn("Product entity is null");
             throw new RuntimeException("Product entity cannot be null");
         }
-
-        // Throw a runtime exception if the user ID is null
-        if (entity.getUserId() == null) {
-            log.warn("The user ID is null");
-            throw new RuntimeException("User ID cannot be null");
+        if (entity.getUser() == null) {
+            log.warn("User is null");
+            throw new RuntimeException("User cannot be null");
         }
     }
 
-    /**
-     * Deletes an existing product entity from the database.
-     * Returns a list of all products with the same user ID as the deleted entity.
-     *
-     * @param entity The product entity to delete.
-     * @return A list of all products with the same user ID as the deleted entity.
-     * @throws RuntimeException if the entity is null or if the ID or user ID is null,
-     *         or if an error occurs while deleting the entity.
-     */
-    public List<ProductEntity> delete(final ProductEntity entity) {
-
-        // Validate the entity before deleting it from the database
-        validate(entity);
-
-        try {
-            // Attempt to delete the entity from the database
-            repository.delete(entity);
-        } catch (Exception e) {
-            // Log the error message with the ID of the entity
-            log.error("Error deleting product with ID " + entity.getId() + ": " + e.getMessage());
-            // Throw a runtime exception with the error message
-            throw new RuntimeException("Error deleting product with ID " + entity.getId() + ": " + e.getMessage());
-        }
-
-        // Return a list of all products with the same user ID as the deleted entity
-        return retrieve(entity.getUserId());
-    }
-
-    // Retrieves a list of all products with the given user ID from the repository
     public List<ProductEntity> retrieve(final String userId) {
         return repository.findByUserId(userId);
     }
