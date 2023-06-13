5db09a492c0a180aee40af1ca2e66c39eba53f60
haneujeen <eujeenhan@gmail.com>
2023-06-14 01:20:23 +0900

    Configure deployment

---
+++ src/main/java/com/example/demo/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Service is up and running!";
+    }
+}
+
+
